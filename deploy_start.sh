a1c64a5c33a97648425cf118a7e4ba527fd805e3
David McCue <david.mccue@dxc.com>
2021-09-24 17:26:23 +0100

    mysql auth

--- deploy_start.sh
+++ deploy_start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-#pip install -r requirements.txt
+export REDIS_AUTH=ComplexPassword123
+export MYSQL_ROOT_PASSWORD=ComplexPassword123
+export MYSQL_DATABASE=gamesample
+export MYSQL_USER=gamesample
+export MYSQL_PASSWORD=gamesample
+
 docker-compose up --build --scale webapp=3
